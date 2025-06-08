59dbad7fea880cee32cf7336791ab5c98342faa0
sircvince <126047093+sircvince@users.noreply.github.com>
2025-06-08 16:00:57 +0800

    Testing 123

--- ConsoleApp.OutputDemo/Program.cs
+++ ConsoleApp.OutputDemo/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bro code!");
+Console.WriteLine("Bro !");
 Console.WriteLine("YOU just kidding man!"); // console is a class ...WriteLine is a method. need parenthese
 Console.Write("1");
 Console.Write("2");
@@ -8,3 +8,4 @@ int x = 1;
 Console.WriteLine();
 Console.WriteLine(x);
 
+Console.WriteLine("TEST");
